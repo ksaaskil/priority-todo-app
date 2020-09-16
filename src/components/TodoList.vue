408bd2429ce324bc6df206cdde89d0aed2cf459b
Kimmo Sääskilahti <kimmo.saaskilahti@silo.ai>
2020-09-16 14:23:58 +0300

    Add vuetify.

---
+++ src/components/TodoList.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="text-h3">
+    TODO
+  </div>
+</template>
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+
+@Component
+export default class TodoList extends Vue {
+  @Prop() private msg!: string;
+}
+</script>
+<style scoped lang="scss">
+.hello {
+  font-size: 2rem;
+}
+</style>
